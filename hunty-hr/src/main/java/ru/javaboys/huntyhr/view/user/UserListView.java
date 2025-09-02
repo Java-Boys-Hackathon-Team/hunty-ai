f4c9df0a79421b8c5269ddfef815402992534cf9
RustamKuramshin <leo.in.empty@gmail.com>
2025-09-03 00:50:06 +0300

    init commit

---
+++ hunty-hr/src/main/java/ru/javaboys/huntyhr/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package ru.javaboys.huntyhr.view.user;
+
+import ru.javaboys.huntyhr.entity.User;
+import ru.javaboys.huntyhr.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
