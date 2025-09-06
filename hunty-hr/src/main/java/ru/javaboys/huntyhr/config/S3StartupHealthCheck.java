ae9184ba1be3d137eb7c6a7091fffc2a029dc45b
RustamKuramshin <leo.in.empty@gmail.com>
2025-09-06 23:11:57 +0300

    Refactor S3 health check to use @PostConstruct, adjust imports, and reorganize build.gradle dependencies

--- hunty-hr/src/main/java/ru/javaboys/huntyhr/config/S3StartupHealthCheck.java
+++ hunty-hr/src/main/java/ru/javaboys/huntyhr/config/S3StartupHealthCheck.java
@@ -1,9 +1,8 @@
 package ru.javaboys.huntyhr.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
@@ -14,7 +13,7 @@ import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
  */
 @Slf4j
 @Component
-public class S3StartupHealthCheck implements ApplicationRunner {
+public class S3StartupHealthCheck {
 
     private final S3Client amazonS3;
     private final String bucketName;
@@ -25,15 +24,15 @@ public class S3StartupHealthCheck implements ApplicationRunner {
         this.bucketName = bucketName;
     }
 
-    @Override
-    public void run(ApplicationArguments args) {
+    @PostConstruct
+    public void verifyS3Availability() {
         try {
             // Perform a lightweight call that requires connectivity and auth
             amazonS3.headBucket(HeadBucketRequest.builder().bucket(bucketName).build());
             log.info("S3 bucket '{}' is accessible. Startup check passed.", bucketName);
         } catch (Exception e) {
             log.error("S3 bucket '{}' is not accessible. Failing application startup.", bucketName, e);
-            // Throwing RuntimeException will stop Spring Boot startup
+            // Throwing RuntimeException will stop Spring Boot startup during context initialization
             throw new IllegalStateException("S3 storage is not available for bucket '" + bucketName + "'", e);
         }
     }
