b0f98ed551841180250db3af5924b4d639898fee
RustamKuramshin <leo.in.empty@gmail.com>
2025-09-10 03:13:19 +0300

    Expand `clear.sh` to also remove virtual environments, UI build artifacts, and node modules.

--- clear.sh
+++ clear.sh
@@ -4,3 +4,8 @@ rm -rf hunty-hr/build
 rm -rf hunty-hr/node_modules
 rm -rf hunty-hr/src/main/frontend/generated
 rm -rf hunty-hr/src/main/bundles
+
+rm -rf hunty-interview-backend/.venv
+
+rm -rf hunty-interview-ui/dist
+rm -rf hunty-interview-ui/node_modules
